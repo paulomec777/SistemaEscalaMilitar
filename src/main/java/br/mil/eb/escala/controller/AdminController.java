4c136aebfc871bc675a10834f834cc5dc1974a77
Paulo Henrique <paulo92176914@gmail.com>
2026-03-03 09:42:34 -0300

    new

--- src/main/java/br/mil/eb/escala/controller/AdminController.java
+++ src/main/java/br/mil/eb/escala/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
 @Controller
