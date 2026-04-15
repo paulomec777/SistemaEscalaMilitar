88922eb058f4db356cb2f56638b4c7de8764ca69
Paulo Henrique <paulo92176914@gmail.com>
2026-04-15 13:06:34 -0300

    Feriado

--- src/main/java/br/mil/eb/escala/model/Feriado.java
+++ src/main/java/br/mil/eb/escala/model/Feriado.java
@@ -1,20 +1,32 @@
 package br.mil.eb.escala.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "feriados")
+@Table(name = "feriados", indexes = {
+    @Index(name = "idx_feriado_data", columnList = "data") 
+})
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Feriado {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDate data; // Ex: 2025-12-25
-    private String descricao; // Ex: Natal
+    @Column(nullable = false, unique = true)
+    private LocalDate data; // Ex: 2026-04-16
+
+    @Column(nullable = false, length = 100)
+    private String descricao; // Ex: DIA DA BANDEIRA
+
+    // Método utilitário para formatar a exibição no sistema/HTML
+    public String getInformativo() {
+        return String.format("%td/%tm - %s", data, data, descricao);
+    }
 }
